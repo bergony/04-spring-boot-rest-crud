997d03924be9800b99431a2f208cc669f160cd5f
bergony.bandeira <bergony.bandeira@quark.tec.br>
2023-07-17 21:52:32 -0300

    Update rest

---
+++ src/main/java/com/bergony/demo/rest/StudentRestController.java
@@ -0,0 +1,32 @@
+package com.bergony.demo.rest;
+
+import com.bergony.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    private List<Student> theStudents = new ArrayList<>();
+
+    @PostConstruct
+    public void loadData() {
+        theStudents.add(new Student("Berony", "bandeira"));
+        theStudents.add(new Student("Jessica", "bandeira"));
+        theStudents.add(new Student("Thrall", "bandeira"));
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
+    @GetMapping("/student/{studendtId}")
+    public Student getStudents(@PathVariable("studendtId") int studendtId) {
+        return theStudents.get(studendtId);
+    }
+}
