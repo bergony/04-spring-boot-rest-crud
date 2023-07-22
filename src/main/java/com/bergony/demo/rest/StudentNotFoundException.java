51086dda4797d279e5eac871b19aa37f0faf9e4f
bergony.bandeira <bergony.bandeira@quark.tec.br>
2023-07-21 21:21:39 -0300

    new projeto

--- src/main/java/com/bergony/demo/rest/StudentNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.bergony.demo.rest;
-
-public class StudentNotFoundException extends RuntimeException{
-
-    public StudentNotFoundException(String message) {
-        super(message);
-    }
-
-}
